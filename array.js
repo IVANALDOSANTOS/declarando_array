74bbdc5230723185f3c02a744ed864c99b6687fb
ivanaldo da silva santos <ivanaldo594@gmail.com>
2024-06-11 18:41:44 -0300

    declarando arrray

---
+++ array.js
@@ -0,0 +1,9 @@
+// Array contendo produtos de uma loja de música
+produtos_musicais = ["Violão", "Piano", "Flauta", "Bateria", "Guitarra"]
+
+// Array contendo anos de nascimento de familiares e amigos
+anos_de_nascimento = [1990, 1985, 2000, 1995, 1988]
+
+// Imprimindo os arrays no terminal
+print("Produtos Musicais:", produtos_musicais)
+print("Anos de Nascimento:", anos_de_nascimento)
